20381fffa583c0dfecc3fc894d4331d6bc984681
FangFoe <fang-pa@hotmail.com>
2023-06-27 21:17:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors())
+
+// เชื่อม Database 
+mongoose.connect(process.env.DB_Connection,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) console.log(err)
+        else console.log("Server is run on http://localhost:3000")
+    })
+mongoose.set('strictQuery', true)
+
+app.get('/', (req, res) => {
+    return res.json({mgs:"Hello World"})
+})
+// Port number for the server
+app.listen(3000);
